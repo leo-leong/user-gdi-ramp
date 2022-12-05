9dc4d4f37ddba20f41ad6c715596fc1791f905db
Leo Leong <leoleong@microsoft.com>
2022-12-05 11:16:36 -0800

    handling keystrokes lab

---
+++ KeyboardInputLab/KeyboardInputLab.cpp
@@ -0,0 +1,163 @@
+#include <windows.h>
+#include <tchar.h>
+
+void HandleKeystrokes(
+    HWND hwnd,
+    UINT uMsg,
+    WPARAM wParam,
+    LPARAM lParam
+)
+{
+    PCTSTR pszMessage = NULL;
+    switch (uMsg)
+    {
+    case WM_SYSKEYDOWN:
+        pszMessage = TEXT("WM_SYSKEYDOWN");
+        break;
+    case WM_SYSKEYUP:
+        pszMessage = TEXT("WM_SYSKEYUP");
+        break;
+    case WM_KEYDOWN:
+        pszMessage = TEXT("WM_KEYDOWN");
+        break;
+    case WM_KEYUP:
+        pszMessage = TEXT("WM_KEYUP");
+        break;
+    default:
+        return;
+    }
+
+    WORD wVk = (WORD)wParam;
+    WORD wRepeatCount = LOWORD(lParam);
+    WORD wScanCode = (WORD)((lParam >> 16) & 0xFF);
+    BOOL fExtendedKey = KF_EXTENDED == (HIWORD(lParam) & KF_EXTENDED);
+    BOOL fAltDown = KF_ALTDOWN == (HIWORD(lParam) & KF_ALTDOWN);
+    BOOL fRepeat = KF_REPEAT == (HIWORD(lParam) & KF_REPEAT);
+    BOOL fKeyUp = KF_UP == (HIWORD(lParam) & KF_UP);
+
+    TCHAR szBuffer[100];
+    int result = _stprintf_s(szBuffer, TEXT("%-13s VK:%-3d SC: %-3d RepeatCount:%-3d Extended:%d AltDown:%d Repeat:%d KeyUp:%d\r\n"),
+        pszMessage,
+        wVk,
+        wScanCode,
+        wRepeatCount,
+        fExtendedKey,
+        fAltDown,
+        fRepeat,
+        fKeyUp
+    );
+    if (-1 != result)
+    {
+        OutputDebugString(szBuffer);
+    }
+}
+
+LRESULT CALLBACK MainWindowProc(
+    HWND hWnd,
+    UINT uMsg,
+    WPARAM wParam,
+    LPARAM lParam
+)
+{
+    PAINTSTRUCT paintStruct;
+    HDC hdc = BeginPaint(hWnd, &paintStruct);
+    if (hdc)
+    {
+        EndPaint(hWnd, &paintStruct);
+    }
+
+    switch (uMsg)
+    {
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(1);
+        break;
+
+    case WM_SYSKEYDOWN:
+        HandleKeystrokes(hWnd, uMsg, wParam, lParam);
+        break;
+    case WM_SYSKEYUP:
+        HandleKeystrokes(hWnd, uMsg, wParam, lParam);
+        break;
+    case WM_KEYDOWN:
+        HandleKeystrokes(hWnd, uMsg, wParam, lParam);
+        break;
+    case WM_KEYUP:
+        HandleKeystrokes(hWnd, uMsg, wParam, lParam);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+int APIENTRY _tWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPTSTR lpCmdLine,
+    _In_ int nShowCmd
+)
+{
+
+    WNDCLASS wc;
+
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = MainWindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = TEXT("MainWindowClass");
+
+
+    if (!RegisterClass(&wc))
+    {
+        return GetLastError();
+    }
+
+    HWND hWndMain = CreateWindowEx(
+        0,
+        TEXT("MainWindowClass"),
+        TEXT("Simple Windows Application"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0,
+        CW_USEDEFAULT, 0,
+        nullptr, nullptr,
+        hInstance, 0
+    );
+    if (!hWndMain)
+    {
+        return GetLastError();
+    }
+
+    ShowWindow(hWndMain, nShowCmd);
+    UpdateWindow(hWndMain);
+
+    MSG msg;
+    BOOL bRet;
+
+    while ((bRet = GetMessage(&msg, nullptr, 0, 0)) != 0)
+    {
+        if (bRet == -1)
+        {
+            // handle the error and possibly exit
+        }
+        else
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+
+
+    return 0;
+}
